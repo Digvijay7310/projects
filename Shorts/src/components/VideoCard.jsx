3520e4334362cdb9a585fd8881747c24ca30a3a1
Digvijay7310 <kumardigvijay377@gmail.com>
2025-11-04 11:18:11 +0530

    Add video Card

--- Shorts/src/components/VideoCard.jsx
+++ Shorts/src/components/VideoCard.jsx
@@ -1,27 +1,10 @@
 import React from "react";
+import videos from '../videos'
 
 const VideoCard = ({ video }) => {
-  return (
-    <div className="video-item">
-      <video
-        src={video.url}
-        muted
-        loop
-        controls={false} // shorts-style look
-        autoPlay
-        style={{
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-        }}
-      />
-
-      <div className="video-info">
-        <h3>{video.title}</h3>
-        <p>{video.description}</p>
-      </div>
-    </div>
-  );
+  return(
+    
+  )
 };
 
 export default VideoCard;
