de3f068fb98d0e17a335e80b43170c82c52b1c41
Digvijay7310 <kumardigvijay377@gmail.com>
2025-11-06 12:46:35 +0530

    Add image files and fix bugs in video Gallery and improve ui of Digital clock

--- search-filter-app/src/components/DigitalClock.jsx
+++ search-filter-app/src/components/DigitalClock.jsx
@@ -35,10 +35,10 @@ function DigitalClock() {
   return (
     <div className='flex justify-center items-center h-screen gap-2 p-2 bg-yellow-950'>
       <div className="h-60 w-full max-w-[600px] rounded-2xl bg-black flex justify-center items-center flex-col shadow-2xl shadow-cyan-300">
-        <i className='text-center text-gray-300 text-5xl'>" Digital Clock "</i>
-        <h2 className='text-white text-4xl font-bold tracking-wider'>Current Time:</h2>
+        <i className='text-center text-gray-300 text-2xl shadow-amber-200 text-shadow-lg '>" Digital Clock "</i>
+        <h2 className='text-white text-xl font-bold tracking-wider'>Current Time:</h2>
         <h6 className='text-[#00ffcc] font-semibold text-[50px]'>{formatTime(time)}</h6>
-        <p className='text-[#f5fe56] font-semibold text-xl'>{formatDate(time)}</p>
+        <p className='text-[#f5fe56] font-semibold text-sm'>{formatDate(time)}</p>
       </div>
     </div>
   );
--- search-filter-app/src/components/VideoGallery.jsx
+++ search-filter-app/src/components/VideoGallery.jsx
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { useState, useEffect, useRef } from 'react';
 
 const VideoGallery = () => {
   const [videos, setVideos] = useState([]);
   const [search, setSearch] = useState('');
 
+  const videoRef = useRef(null)
+
   useEffect(() => {
-    const res = fetch('https://example.com/api/videos')
-      .then(res => setVideos(res.data))
-      .catch(err => console.log(err));
+    fetch('https://raw.githubusercontent.com/Digvijay7310/Video-Data/refs/heads/main/videos.json')
+      .then(res => res.json())
+      .then(data => {
+        setVideos(data.data); // your JSON has a "data" array
+      })
+      .catch(err => console.error("Fetch error:", err));
   }, []);
 
   const filtered = videos.filter(v =>
@@ -16,27 +20,41 @@ const VideoGallery = () => {
   );
 
   return (
-    <div className="p-4 flex flex-col justify-center items-center h-screen bg-red-100">
-      <h2>Video Search</h2>
+    <div className="p-1 flex flex-col justify-center items-center min-h-screen bg-red-100">
+      <h2 className='text-2xl tracking-widest mb-4'>Video Search</h2>
+
       <input
         type="text"
         placeholder="Search by title..."
         value={search}
         onChange={(e) => setSearch(e.target.value)}
-        className="border p-2 rounded"
+        className="border p-2 rounded w-full max-w-[800px]"
       />
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-3 gap-1 mt-4">
         {filtered.length > 0 ? (
-          filtered.map((v, i) => (
-            <div key={i} className="bg-white shadow rounded p-2">
-              <h4>{v.title}</h4>
-              <video width="100%" controls src={v.url}></video>
-            </div>
-          ))
-        ) : (
-          <p>No videos found.</p>
-        )}
+  filtered.map((v) => (
+    <div key={v.id} className="bg-gray-100 flex flex-col justify-center items-center shadow rounded-xl p-1">
+      <h4 className="font-medium text-xs sm:text-sm mb-1 text-center line-clamp-1">{v.title}</h4>
+      <video
+        onMouseOver={(e) => e.currentTarget.play()}
+        onMouseLeave={(e) => {
+          e.currentTarget.pause();
+          e.currentTarget.currentTime = 0;
+        }}
+        className='w-40 md:w-full h-20 md:h-50 rounded-lg object-contain'
+        muted
+        playsInline
+      >
+        <source src={v.url} type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  ))
+) : (
+  <p>No videos found.</p>
+)}
+
       </div>
     </div>
   );
